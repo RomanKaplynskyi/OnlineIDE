552fb867bad17836c48235cbe1d58f570ad9f43e
Roman <43010982+RomanKaplynskyi@users.noreply.github.com>
2020-11-16 20:45:54 +0200

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
+import * as cors from '@koa/cors'
+import * as serve from 'koa-static'
+const app = new Koa();
+const router = new Router();
+const PORT = process.env.PORT || 8080;
+app.use(serve('./dist'))
+app.use(bodyParser());
+app.use(cors())
+app.use(router.routes());
+
+router.get('/', async (ctx) => { })
+
+app.listen(PORT, () => {
+  console.log('Koa started!');
+});
