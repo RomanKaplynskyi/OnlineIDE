54259b34e1f9206ce462fdf4059a6fd996b106c1
Roman <43010982+RomanKaplynskyi@users.noreply.github.com>
2020-11-16 20:52:40 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app id="app">
+    <side-menu />
+    <v-main>
+    </v-main>
+    <v-footer app>
+    </v-footer>
+  </v-app>
+</template>
+
+<script >
+export default class App extends Vue {}
+</script>
